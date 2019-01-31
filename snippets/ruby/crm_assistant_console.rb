29ff3971dc01a5bb5d2883284e3d7a0d4cf43831
Giovanni Benussi <giovanni.benussi@usach.cl>
2019-01-31 20:11:37 -0300

    Last Changes

--- snippets/ruby/crm_assistant_console.rb
+++ snippets/ruby/crm_assistant_console.rb
@@ -3,5 +3,6 @@ require 'awesome_print'
 crm_user = User.find_by(email: 'giovanni.benussi@usach.cl')&.crm_user
 
 "giovanni.benussi@usach.cl exists?: #{!crm_user.nil?}"
+http://propertysimple.com/giovanni-benussi/articles/test-slug-308
 
 trigger
